22c2f8db093bf9e1b36ba1de83d010ac0eece100
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-14 11:21:34 -0700

    Initial Commit

---
+++ Rotten Mangoes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rotten Mangoes
+//
+//  Created by asu on 2015-09-14.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
